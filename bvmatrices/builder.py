428257ab7f3dc2092baf671fe2ec74f1aafa4362
Cirdans-Home <f.durastante@na.iac.cnr.it>
2022-02-25 18:52:25 +0100

    Completed build of GBDF matrices

--- bvmatrices/builder.py
+++ bvmatrices/builder.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.sparse import coo_matrix
 
 def vsolve(x,b):
     # This function solves the Vandermonde linear system W(x)f = b;
@@ -35,7 +36,58 @@ def mab(type,k,n):
         print("Building TOM matrices")
     elif type.upper() == "GBDF":
         print("Building GBDF matrices")
+        nu = int(np.fix((k+2)/2))
+        a_irow = np.zeros(shape=(k+1)*n,dtype=int)
+        a_icol = np.zeros(shape=(k+1)*n,dtype=int)
+        a_ival = np.zeros(shape=(k+1)*n,dtype=float)
+        b_irow = np.zeros(shape=(k+1)*n,dtype=int)
+        b_icol = np.zeros(shape=(k+1)*n,dtype=int)
+        b_ival = np.zeros(shape=(k+1)*n,dtype=float)
+        row = 0
+        # We start with the entries relative to boundary conditions on the left
+        for i in np.arange(0,nu):
+            [ro,si] = rosi_gbdf(k,i+1)
+            for j in np.arange(0,k+1):
+                a_irow[row] = i
+                a_icol[row] = j
+                a_ival[row] = ro[j]
+                b_irow[row] = i
+                b_icol[row] = j
+                b_ival[row] = si[j]
+                row = row + 1
+        # Then we populate the central part of the matrix
+        for i in np.arange(nu,n-(k-nu)):
+            col = 0
+            for j in np.arange(-nu,k-nu+1):
+                a_irow[row] = i
+                a_icol[row] = i+1+j
+                a_ival[row] = ro[col]
+                b_irow[row] = i
+                b_icol[row] = i+1+j
+                b_ival[row] = si[col]
+                row = row + 1
+                col = col + 1
+        # Finally we put in place boundary conditions on the right
+        l = nu
+        for i in np.arange(n-(k-nu),n):
+            l = l + 1
+            [ro,si] = rosi_gbdf(k,l)
+            col = 0
+            for j in np.arange(-k,1):
+                a_irow[row] = i
+                a_icol[row] = n+j
+                a_ival[row] = ro[col]
+                print("3] a("+str(a_irow[row])+","+str(a_icol[row])+")="+str(a_ival[row]))
+                b_irow[row] = i
+                b_icol[row] = n+j
+                b_ival[row] = si[col]
+                row = row + 1
+                col = col + 1
+        A = coo_matrix((a_ival,(a_irow,a_icol)),(n,n+1))
+        B = coo_matrix((b_ival,(b_irow,b_icol)),(n,n+1))
     elif type.upper() == "GAM":
         print("Building GAM matrices")
     else:
         raise NameError('Unknown formula')
+
+    return A,B
